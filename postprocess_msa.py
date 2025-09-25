d2db91b4ce9e81ecf7f983b2adbaa8e86075d870
volkanozsarac <volkan.ozsarac@iusspavia.it>
2025-09-25 15:04:36 +0200

    add poe 0.4

--- postprocess_msa.py
+++ postprocess_msa.py
@@ -7,8 +7,8 @@ from src.utilities import export_results
 msa_path = Path(__file__).parent / "outs"
 
 
-poes = [0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.0025, 0.001, 0.0004, 0.0002, 0.0001]
-imls = [0.248, 0.377, 0.541, 0.822, 1.08, 1.4, 1.75, 2.29, 2.91, 3.4, 3.98]
+poes = [0.4, 0.2, 0.1, 0.05, 0.02, 0.01, 0.005, 0.0025, 0.001, 0.0004, 0.0002, 0.0001]
+imls = [0.148, 0.248, 0.377, 0.541, 0.822, 1.08, 1.4, 1.75, 2.29, 2.91, 3.4, 3.98]
 inv_t = 50
 return_period = np.round(-inv_t / np.log(1 - np.array(poes))).astype(int).tolist()
 model = MSAPostprocessor(msa_path)
